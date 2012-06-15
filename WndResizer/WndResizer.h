de2c0cae749b4e7df16927610cd7bab5ef77012f
clbs <cpounds@datafunctions.com>
2012-06-15 18:27:39 -0500

    Initial commit

---
+++ WndResizer/WndResizer.h
@@ -0,0 +1,32 @@
+
+// WndResizer.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CWndResizerApp:
+// See WndResizer.cpp for the implementation of this class
+//
+
+class CWndResizerApp : public CWinApp
+{
+public:
+	CWndResizerApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CWndResizerApp theApp;
