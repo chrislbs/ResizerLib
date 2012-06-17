b0b336839c6426533b1f16cc202e517b49914e49
clbs <cpounds@datafunctions.com>
2012-06-17 00:23:40 -0500

    Removing ANCHOR_*_CENTERED because default impl does just that

--- WndResizer/panel.cpp
+++ WndResizer/panel.cpp
@@ -41,12 +41,7 @@ void CPanel::OnResized()
 		const RECT& cr = p->GetRect();
 
 		RECT r = { 0 };
-		if(anchor & ANCHOR_HORZ_CENTERED)
-		{
-			r.left = pr.left + (static_cast<int>( (GetRectWidth(pr) - GetRectWidth(cr)) / 2));
-			r.right = r.left + GetRectWidth(cr);
-		}
-		else if((anchor & ANCHOR_HORZ) == ANCHOR_HORZ)
+		if((anchor & ANCHOR_HORZ) == ANCHOR_HORZ)
 		{
 			r.left = pr.left + off.left;
 			r.right = pr.right - off.right;
@@ -69,12 +64,7 @@ void CPanel::OnResized()
 			r.right = r.left + GetRectWidth(cr);
 		}
 
-		if(anchor & ANCHOR_VERT_CENTERED)
-		{
-			r.top = pr.top + (static_cast<int>( (GetRectWidth(pr) - GetRectWidth(cr)) / 2));
-			r.bottom = r.top + GetRectHeight(cr);
-		}
-		else if((anchor & ANCHOR_VERT) == ANCHOR_VERT)
+		if((anchor & ANCHOR_VERT) == ANCHOR_VERT)
 		{
 			r.top = pr.top + off.top;
 			r.bottom = pr.bottom - off.bottom;
