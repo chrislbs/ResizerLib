27110dca9f632ce63feb7d7029ba8acaf087b85a
clbs <cpounds@datafunctions.com>
2012-06-17 12:59:40 -0500

    Adding a base CWnd class for MFC builds to use for auto resizing.

---
+++ ResizerLib/ResizingWnd.cpp
@@ -0,0 +1,70 @@
+#include "stdafx.h"
+#include "ResizingWnd.h"
+#include "rootwndpanel.h"
+
+#ifdef _AFXDLL
+
+IMPLEMENT_DYNAMIC(CResizingWnd, CWnd)
+
+const TCHAR CResizingWnd::CLASS_NAME[] = "RESIZING_WINDOW_CLASS";
+
+CResizingWnd::CResizingWnd()
+:m_rootPanel(NULL)
+{
+}
+
+BOOL CResizingWnd::RegisterWindowClass()
+{
+	WNDCLASS wndcls;
+    HINSTANCE hInst = AfxGetInstanceHandle();
+
+    if (!(::GetClassInfo(hInst, CLASS_NAME, &wndcls)))
+    {
+        // otherwise we need to register a new class
+        wndcls.style            = CS_DBLCLKS | CS_HREDRAW | CS_VREDRAW;
+        wndcls.lpfnWndProc      = ::DefWindowProc;
+        wndcls.cbClsExtra       = wndcls.cbWndExtra = 0;
+        wndcls.hInstance        = hInst;
+        wndcls.hIcon            = NULL;
+        wndcls.hCursor          = AfxGetApp()->LoadStandardCursor(IDC_ARROW);
+        wndcls.hbrBackground    = (HBRUSH) (COLOR_3DFACE + 1);
+        wndcls.lpszMenuName     = NULL;
+        wndcls.lpszClassName    = CLASS_NAME;
+
+        if (!AfxRegisterClass(&wndcls))
+        {
+            AfxThrowResourceException();
+            return FALSE;
+        }
+    }
+
+    return TRUE;
+}
+
+void CResizingWnd::PreSubclassWindow()
+{
+	CWnd::PreSubclassWindow();
+	
+	m_rootPanel = new CRootWndPanel(GetSafeHwnd());
+}
+
+BEGIN_MESSAGE_MAP(CResizingWnd, CWnd)
+	ON_WM_SIZE()
+	ON_WM_SIZING()
+END_MESSAGE_MAP()
+
+void CResizingWnd::OnSize(UINT nType, int cx, int cy)
+{
+	CWnd::OnSize(nType, cx, cy);
+
+	ResizePanels(this, cx, cy);
+	ResizeWndPanels(this);
+}
+
+void CResizingWnd::OnSizing(UINT fwSide, LPRECT pRect)
+{
+	CWnd::OnSizing(fwSide, pRect);
+	ValidateWindowSizing(this, fwSide, pRect);
+}
+
+#endif // is def _AFXDLL
---
+++ ResizerLib/ResizingWnd.h
@@ -0,0 +1,37 @@
+#ifndef _resizing_wnd_h_
+#define _resizing_wnd_h_
+
+#ifdef _AFXDLL
+
+#include "resizablewnd.h"
+
+class CResizingWnd : public CWnd, public IResizableWnd
+{
+	DECLARE_DYNAMIC(CResizingWnd)
+
+	CRootWndPanel * m_rootPanel;
+
+public:
+	static const TCHAR CLASS_NAME[];
+
+	CResizingWnd();
+
+	inline virtual CRootWndPanel * GetRootPanel() { return m_rootPanel; }
+
+	// CWnd Overrides
+	virtual void PreSubclassWindow();
+
+	// MFC Messages
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnSizing(UINT fwSide, LPRECT pRect);
+
+private:
+	BOOL RegisterWindowClass();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#endif // is def _AFXDLL
+
+#endif
