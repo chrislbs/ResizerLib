f8fc2f14ae3df69c9f4bb7cff43e23b3baa07be8
clbs <cpounds@datafunctions.com>
2012-06-18 18:12:34 -0500

    Added real template magic to panel funcs based on SFINAE concept

--- ResizerLib/panelfuncs.h
+++ ResizerLib/panelfuncs.h
@@ -6,22 +6,17 @@
 template<typename InputIterator, typename OutputIterator>
 void CreateWndPanels(InputIterator begin, InputIterator end, OutputIterator out, UINT anchor = 0)
 {
-	_CreateWndPanels(
-		begin, end, out, anchor,
-		typename std::iterator_traits<InputIterator>::iterator_category(),
-		typename std::iterator_traits<InputIterator>::value_type(),
-		typename std::iterator_traits<OutputIterator>::iterator_category());
+	_CreateWndPanels(begin, end, out, anchor);
 }
 
+// this is the coolest magic i've ever seen
 template<typename InputIterator, typename OutputIterator>
-void _CreateWndPanels(
-	InputIterator begin,
-	InputIterator end,
-	OutputIterator out,
-	UINT anchor,
-	typename std::input_iterator_tag,
-	typename CWnd,
-	typename std::output_iterator_tag)
+typename std::enable_if<
+	std::is_base_of<CWnd, typename std::iterator_traits<InputIterator>::value_type>::value &&
+	std::is_base_of<std::input_iterator_tag, typename std::iterator_traits<InputIterator>::iterator_category>::value &&
+	std::is_base_of<std::output_iterator_tag, typename std::iterator_traits<OutputIterator>::iterator_category>::value
+>::type
+_CreateWndPanels(InputIterator begin, InputIterator end, OutputIterator out, UINT anchor)
 {
 	while(begin != end)
 	{
@@ -34,19 +29,19 @@ void _CreateWndPanels(
 template<typename InputIterator>
 void SetPanelsParent(CPanel * parent, InputIterator begin, InputIterator end)
 {
-	_SetPanelsParent(
-		parent, begin, end,
-		typename std::iterator_traits<InputIterator>::iterator_category(),
-		typename std::iterator_traits<InputIterator>::value_type());
+	_SetPanelsParent(parent, begin, end);
 }
 
+// more template magic
 template<typename InputIterator>
-void _SetPanelsParent(
+typename std::enable_if<
+	std::is_convertible<typename std::iterator_traits<InputIterator>::value_type, CPanel *>::value &&
+	std::is_base_of<std::input_iterator_tag, typename std::iterator_traits<InputIterator>::iterator_category>::value
+>::type
+_SetPanelsParent(
 	CPanel * parent,
 	InputIterator begin, 
-	InputIterator end,
-	typename std::input_iterator_tag,
-	typename CPanel *)
+	InputIterator end)
 {
 	while(begin != end)
 	{
