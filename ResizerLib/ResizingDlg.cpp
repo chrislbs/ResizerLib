a56ab712995423a79614c5fc25a13239c38c7cae
clbs <cpounds@datafunctions.com>
2012-06-18 13:39:31 -0500

    Adding new MFC Resizing Dialog implementation

---
+++ ResizerLib/ResizingDlg.cpp
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+#include "ResizingDlg.h"
+#include "rootwndpanel.h"
+
+#ifdef _AFXDLL
+
+IMPLEMENT_DYNAMIC(CResizingDlg, CDialog)
+
+CResizingDlg::CResizingDlg(UINT IDD, CWnd * pParent)
+	:CDialog(IDD, pParent), m_rootPanel(NULL)
+{
+}
+
+BOOL CResizingDlg::OnInitDialog()
+{
+	BOOL ret = CDialog::OnInitDialog();
+
+	m_rootPanel = new CRootWndPanel(GetSafeHwnd());
+
+	return ret;
+}
+
+BEGIN_MESSAGE_MAP(CResizingDlg, CDialog)
+	ON_WM_DESTROY()
+	ON_WM_SIZE()
+	ON_WM_SIZING()
+END_MESSAGE_MAP()
+
+void CResizingDlg::OnSize(UINT nType, int cx, int cy)
+{
+	CDialog::OnSize(nType, cx, cy);
+
+	ResizePanels(this, cx, cy);
+	ResizeWndPanels(this);
+}
+
+void CResizingDlg::OnSizing(UINT fwSide, LPRECT pRect)
+{
+	CDialog::OnSizing(fwSide, pRect);
+	ValidateWindowSizing(this, fwSide, pRect);
+}
+
+void CResizingDlg::OnDestroy()
+{
+	CDialog::OnDestroy();
+	m_rootPanel->OnDestroy();
+}
+
+#endif
---
+++ ResizerLib/ResizingDlg.h
@@ -0,0 +1,30 @@
+#ifndef _resizing_dlg_h_
+#define _resizing_dlg_h_
+
+#ifdef _AFXDLL
+
+#include "resizablewnd.h"
+
+class CResizingDlg : public CDialog, public IResizableWnd
+{
+	DECLARE_DYNAMIC(CResizingDlg)
+
+	CRootWndPanel * m_rootPanel;
+
+public:
+	CResizingDlg(UINT IDD, CWnd * pParent = NULL);
+
+	inline virtual CRootWndPanel * GetRootPanel() { return m_rootPanel; }
+
+protected:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnSizing(UINT fwSide, LPRECT pRect);
+	afx_msg void OnDestroy();
+
+	DECLARE_MESSAGE_MAP()
+};
+
+#endif // is def _AFXDLL
+
+#endif
