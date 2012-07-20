1863e265f1c7890ba4d4aa656da988317bdbd3e6
clbs <cpounds@datafunctions.com>
2012-07-19 19:43:00 -0500

    Root wnd panels will set their rect to be the client rect in constructor

--- ResizerLib/rootwndpanel.cpp
+++ ResizerLib/rootwndpanel.cpp
@@ -7,6 +7,10 @@ CRootWndPanel::CRootWndPanel(HWND hWnd, SIZE szMin)
 	:CWndPanel(hWnd)
 {
 	memcpy_s(&m_szMin, sizeof(SIZE), &szMin, sizeof(SIZE));
+
+	RECT cr;
+	GetClientRect(hWnd, &cr);
+	SetRect(cr);
 }
 
 CRootWndPanel::~CRootWndPanel()
